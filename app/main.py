b2e1a668fc26e72a1fb4807fc577f2f3ea2e66cd
mayur <mayurreddy5@gmail.com>
2025-08-10 15:06:29 +0530

    secondcommit

--- app/main.py
+++ app/main.py
@@ -1,150 +1,108 @@
-from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel, Field, computed_field as ComputedField
-from typing import Annotated, Literal, Optional
-import json
+from pydantic import BaseModel, Field, computed_field,field_validator
+from typing import Literal
+import pickle
+import pandas as pd
+
+# Load the model
+with open("model/model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+MODEL_VERSION='1.0.0'
 
 app = FastAPI()
 
+tier_1_cities = ["Mumbai", "Delhi", "Bangalore", "Chennai", "Pune", "Kolkata", "Hyderabad"]
+tier_2_cities = [
+    "Ahmedabad", "Jaipur", "Lucknow", "Kanpur", "Nagpur", "Indore", "Coimbatore",
+    "Patna", "Bhopal", "Vadodara", "Visakhapatnam", "Surat", "Rajkot", "Agra",
+    "Meerut", "Thane", "Nashik", "Vijayawada", "Aurangabad", "Madurai", "Jodhpur",
+    "Amritsar", "Allahabad", "Varanasi", "Srinagar", "Gwalior", "Raipur", "Ranchi",
+    "Dehradun", "Mysore", "Jamshedpur", "Udaipur", "Solapur", "Dhanbad", "Asansol",
+    "Nanded", "Ajmer", "Bhilai", "Bikaner", "Guwahati", "Faridabad", "Kalyan",
+    "Vasai-Virar", "Siliguri", "Jalandhar", "Ulhasnagar", "Bhubaneswar", "Warangal",
+    "Nellore", "Guntur", "Amravati", "Cuttack", "Kochi", "Thiruvananthapuram",
+    "Mangalore", "Jammu", "Raigarh", "Durgapur", "Bardhaman", "Kota", "Bilaspur",
+    "Shimla", "Rourkela", "Kakinada", "Tiruchirappalli", "Ujjain", "Sagar", "Jhansi",
+    "Gaya", "Bareilly", "Moradabad", "Aligarh", "Bhatinda", "Tirupati", "Nizamabad",
+    "Kurnool", "Chandrapur", "Bhilwara", "Buxar", "Hapur", "Saharanpur",
+    "Muzaffarpur", "Bardoli", "Kharagpur", "Shillong"
+]
 
-# ----------------------- MODELS ----------------------- #
+# Pydantic model to validate incoming data
+class UserInput(BaseModel):
+    age: int = Field(..., gt=0, description="Age of User")
+    weight: float = Field(..., gt=0, description="Weight in kg")
+    height: float = Field(..., gt=0, description="Height in meters")
+    income_lpa: float = Field(..., gt=0, description="Income in lakhs per annum")
+    smoker: Literal['yes', 'no'] = Field(..., description="Smoking status")
+    city: str = Field(..., description="City of residence")
+    occupation: Literal['retired', 'freelancer', 'student', 'government_job', 'private_job', 'unemployed', 'businessman'] = Field(..., description="Occupation type")
+    
+    @field_validator('city')
+    @classmethod
+    def normalize_city(cls,v:str)->str:
+        v=v.strip().title()
 
-class Patient(BaseModel):
-    id: Annotated[int, Field(..., title="Patient ID", description="The ID of the patient")]
-    name: Annotated[str, Field(..., title="Patient Name", description="The name of the patient")]
-    city: Annotated[str, Field(..., title="Patient City", description="The city where the patient resides")]
-    age: Annotated[int, Field(..., title="Patient Age", description="The age of the patient")]
-    gender: Annotated[Literal['male', 'female', 'other'], Field(..., description="Gender of patient")]
-    height: Annotated[float, Field(..., gt=0, title="Patient Height", description="Height in cm")]
-    weight: Annotated[float, Field(..., gt=0, title="Patient Weight", description="Weight in kg")]
-
-    @ComputedField
+    @computed_field
     @property
     def bmi(self) -> float:
-        bmi = self.weight / ((self.height / 100) ** 2)
-        return round(bmi, 2)
+        return self.weight / (self.height ** 2)
 
-    @ComputedField
+    @computed_field
     @property
-    def verdict(self) -> str:
-        if self.bmi < 18.5:
-            return "underweight"
-        elif 18.5 <= self.bmi < 24.9:
-            return "normal"
-        elif 25 <= self.bmi < 29.9:
-            return "overweight"
+    def lifestyle_risk(self) -> str:
+        if self.smoker == 'yes' and self.bmi > 30:
+            return "high"
+        elif self.smoker == 'yes' or self.bmi > 27:
+            return "medium"
         else:
-            return "obese"
+            return "low"
 
+    @computed_field
+    @property
+    def age_group(self) -> str:
+        if self.age < 25:
+            return "young"
+        elif self.age < 45:
+            return "middle-aged"
+        elif self.age < 60:
+            return "middle_aged"
+        return "senior"
 
-class PatientUpdate(BaseModel):
-    name: Annotated[Optional[str], Field(default=None)]
-    city: Annotated[Optional[str], Field(default=None)]
-    age: Annotated[Optional[int], Field(default=None, gt=0)]
-    gender: Annotated[Optional[Literal['male', 'female', 'other']], Field(default=None)]
-    height: Annotated[Optional[float], Field(default=None, gt=0)]
-    weight: Annotated[Optional[float], Field(default=None, gt=0)]
+    @computed_field
+    @property
+    def city_tier(self) -> int:
+        if self.city in tier_1_cities:
+            return 1
+        if self.city in tier_2_cities:
+            return 2
+        return 3
 
-
-# ----------------------- DATA UTILS ----------------------- #
-
-def load_data():
-    try:
-        with open("patient.json", "r") as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return []  # return empty list if file doesn't exist
-
-
-def save_data(data):
-    with open("patient.json", "w") as f:
-        json.dump(data, f, indent=4)
-
-
-# ----------------------- ROUTES ----------------------- #
-
-@app.get("/")
+#home endpoint
+@app.get('/')
 def home():
-    return {"message": "Patient Management System API"}
+    return{'home':"Insurance prediction api"}
 
+#healthcheck endpoint
+@app.get('/health')
+def health_check():
+    return{
+        'status':'OK',
+        'version':MODEL_VERSION
+    }
 
-@app.get("/about")
-def about():
-    return {"message": "Fully functional API to manage patient records"}
+@app.post("/predict")
+def predict_premium(data: UserInput):
+    input_df = pd.DataFrame([{
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation,
+        'lifestyle_risk':data.lifestyle_risk
+    }])
 
-
-@app.get("/view")
-def view_all():
-    return load_data()
-
-
-@app.get("/patient/{id}")
-def view_patient(id: int = Path(..., title="Patient ID")):
-    data = load_data()
-    for patient in data:
-        if patient["id"] == id:
-            return patient
-    raise HTTPException(status_code=404, detail="Patient not found")
-
-
- #by height-http://127.0.0.1:8000/sort?sort_by=height&order=asc 
- #by name-http://127.0.0.1:8000/sort?sort_by=name&order=desc 
- #by id-http://127.0.0.1:8000/sort?sort_by=id&order=asc
-
-@app.get("/sort")  
-
-def sort_patients(
-    sort_by: str = Query(..., title="Sort By", description="Sort by 'height', 'name', or 'id'"),
-    order: str = Query("asc", title="Order", description="Sort order: 'asc' or 'desc'")
-):
-    valid_fields = ["height", "name", "id"]
-    if sort_by not in valid_fields:
-        raise HTTPException(status_code=400, detail=f"Invalid sort field. Choose from {valid_fields}")
-    if order not in ["asc", "desc"]:
-        raise HTTPException(status_code=400, detail="Invalid order. Use 'asc' or 'desc'")
-
-    data = load_data()
-    reverse = (order == "desc")
-    return sorted(data, key=lambda x: x.get(sort_by, 0), reverse=reverse)
-
-
-@app.post("/create")
-def create_patient(patient: Patient):
-    data = load_data()
-    if any(p["id"] == patient.id for p in data):
-        raise HTTPException(status_code=400, detail="Patient with this ID already exists")
-    data.append(patient.model_dump())
-    save_data(data)
-    return JSONResponse(status_code=201, content={"message": "Patient created successfully"})
-
-
-@app.put("/edit/{patient_id}")
-def update_patient(patient_id: int, patient_update: PatientUpdate):
-    data = load_data()
-
-    for index, patient in enumerate(data):
-        if patient["id"] == patient_id:
-            updated_info = patient_update.model_dump(exclude_unset=True)
-            data[index].update(updated_info)
-
-            # Validate and recompute BMI & verdict
-            validated = Patient(**data[index])
-            data[index] = validated.model_dump()
-
-            save_data(data)
-            return JSONResponse(status_code=200, content={"message": "Patient updated successfully"})
-
-    raise HTTPException(status_code=404, detail="Patient not found")
-
-@app.delete("/delete/{patient_id}")
-def delete_patient(patient_id: int):
-    data = load_data()
-
-    for index, patient in enumerate(data):
-        if patient["id"] == patient_id:
-            del data[index]
-            save_data(data)
-            return JSONResponse(status_code=200, content={"message": "Patient deleted successfully"})
-
-    raise HTTPException(status_code=404, detail="Patient not found")
-
+    prediction = model.predict(input_df)[0]
+    return JSONResponse(status_code=200, content={'predicted_category': prediction})
